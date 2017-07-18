9be63d1291975d30c241f281d25e185077bfe2d3
Kendrickonly <kendrickserg@gmail.com>
2017-07-18 16:09:23 +0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -2,19 +2,17 @@
 
 class mymail {
     
-    private $pochta;
+    private $pochta = "yontvid@mail.ru";
     private $subject;
     private $message;
-    private $headers = "From: yontvid@mail.ru";
     
-    public function __construct($pochta, $subject, $message) {
-        $this->pochta = $pochta;
+    public function __construct($pochta, $name, $subject, $message) {
         $this->subject = $subject;
-        $this->message = $message;
+        $this->message = "Имя: ".$name."\n\r"."E-mail: ".$pochta."\n\r"."Тема: ".$subject."\n\r"."Сообщение: \n\r".$message;
     }
     
     public function send() {
-        if(mail($this->pochta, $this->subject, $this->message, $this->headers)) {
+        if(mail($this->pochta, $this->subject, $this->message)) {
             return true;
         } else {
             return false;
@@ -22,11 +20,12 @@ class mymail {
     }
 }
 
+$pochta = trim(htmlspecialchars($_POST['Name']));
 $pochta = trim(htmlspecialchars($_POST['Email']));
 $subject = trim(htmlspecialchars($_POST['Subject']));
 $message = trim(htmlspecialchars($_POST['Message']));
 
-$mail = new mymail($pochta, $subject, $message);
+$mail = new mymail($pochta, $name, $subject, $message);
 
 exit($mail->send());
 
